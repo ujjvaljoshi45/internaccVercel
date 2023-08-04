4806d3479a06738e6d59700c8ce9828d877d977c
Ujjval Joshi <ujjvaljoshi45@gmail.com>
2023-08-04 17:23:24 +0530

    c done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,7 +7,7 @@ function Carousel() {
                 <h1 className="text-light text-center " style={{ fontSize: "32px" }}>Accelerate Your career with world's Best University's</h1>
                 <h5 className='text-center text-secondary mt-md-0 mt-3' style={{ fontSize: "20px" }}>Choose from 7 Existing University</h5>
                 <marquee behavior="scroll" direction="" loop={true} scrollamount="15">
-                    <div className=" mt-5" style={{ display: "flex", flexShrink: 0, flexGrow: 3, gap: 100 }}>
+                    <div className=" mt-5" style={{width: "1300px", display: "flex", flexShrink: 0, flexGrow: 3, gap: 100 }}>
                         <div className="university-card d-flex align-items-end justify-content-center" style={{ width: "400px", height: "400px", backgroundImage: "url('https://adultlifestylecommunities.com/wp-content/uploads/2022/09/Untitled-design-4.png')", backgroundRepeat: "no-repeat", backgroundAttachment: "scroll", backgroundSize: "cover", backgroundPosition: "center", borderRadius: "15px" }}>
                             <h3 className='text-center text-light mb-3'>Oxford</h3>
                         </div>
